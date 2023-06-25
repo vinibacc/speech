4f74d978f9a662fd770ac6a5b190b7d615066af3
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2023-06-25 18:47:19 -0300

    STYLE.CSS E COMENTS

--- speech.js
+++ speech.js
@@ -10,6 +10,7 @@ const toggleButton = document.querySelector('#toggleButton');
 const main = document.querySelector('main');
 
 toggleButton.addEventListener('click', () => {
+  // Função associada ao evento de clique no botão de toggle
   textarea.classList.toggle('expanded');
   header.classList.toggle('hidden');
   voices.classList.toggle('hidden');
@@ -17,12 +18,13 @@ toggleButton.addEventListener('click', () => {
   '<i class="bi bi-arrows-angle-contract"></i>' :
   '<i class="bi bi-arrows-angle-expand"></i>';
 
-
   if (textarea.classList.contains('expanded')) {
+    // Se o textarea estiver expandido, ajusta o layout da página
     document.body.style.overflow = 'hidden'; // Impede a rolagem da página quando o textarea estiver expandido
     main.classList.remove('container', 'mt-5');
     main.classList.add('container-fluid');
   } else {
+    // Se o textarea não estiver expandido, restaura o layout original da página
     main.classList.remove('container-fluid');
     main.classList.add('container', 'mt-5');
     document.body.style.overflow = ''; // Habilita a rolagem da página novamente
@@ -30,6 +32,7 @@ toggleButton.addEventListener('click', () => {
 });
 
 function populateVoicesList() {
+  // Função responsável por popular a lista de vozes disponíveis para síntese de fala
   let voicesList = window.speechSynthesis.getVoices();
   voices.innerHTML = '';
 
@@ -42,20 +45,25 @@ function populateVoicesList() {
 }
 
 function togglePlayPause() {
+  // Função responsável por alternar entre pausar/reproduzir a síntese de fala
   if (utterance !== null) {
     if (window.speechSynthesis.paused) {
+      // Se a síntese estiver pausada, retoma a reprodução
       window.speechSynthesis.resume();
       playPauseButton.innerHTML = "<i class='bi bi-pause'></i>";
     } else if (window.speechSynthesis.speaking) {
+      // Se a síntese estiver em andamento, pausa a reprodução
       window.speechSynthesis.pause();
       playPauseButton.innerHTML = "<i class='bi bi-play-fill'></i>";
     }
   } else {
+    // Se não houver uma síntese em andamento, inicia a reprodução
     speakText();
   }
 }
 
 function speakText() {
+  // Função responsável por iniciar a síntese de fala com o texto do textarea
   if (textarea.value !== '') {
     let voicesList = window.speechSynthesis.getVoices();
     utterance = new SpeechSynthesisUtterance(textarea.value);
@@ -66,6 +74,7 @@ function speakText() {
 }
 
 function stopSpeech() {
+  // Função responsável por parar a síntese de fala em andamento
   if (utterance !== null) {
     window.speechSynthesis.cancel();
     utterance = null;
@@ -77,10 +86,12 @@ playPauseButton.addEventListener('click', togglePlayPause);
 stopButton.addEventListener('click', stopSpeech);
 
 voices.addEventListener('change', () => {
+  // Função associada ao evento de alteração da seleção de vozes
   selectedVoice = parseInt(voices.value);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Função executada quando o conteúdo do DOM é carregado
   if ('speechSynthesis' in window) {
     window.speechSynthesis.addEventListener('voiceschanged', populateVoicesList);
   } else {
