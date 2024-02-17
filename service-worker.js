67ae3359490b10a56814cbe2906446252cb24e27
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2024-02-17 20:19:31 -0300

    manifest

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('meu-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/icone.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
